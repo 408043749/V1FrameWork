9495578c28a2ac25a1f1db75499871114e5868e1
408043749@qq.com <408043749@qq.com>
2013-07-31 18:10:10 +0800

    20130731

--- springSecurity/src/main/java/com/panlj/system/service/impl/UserServiceImpl.java
+++ springSecurity/src/main/java/com/panlj/system/service/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.panlj.system.service.impl;
 
+import org.springframework.flex.remoting.RemotingDestination;
 import org.springframework.stereotype.Service;
 
 import com.panlj.system.service.UserService;
 
 
 @Service
+@RemotingDestination
 public class UserServiceImpl implements UserService{
-
+	public String helloWorld(){
+		return "hellow";
+	}
 }
